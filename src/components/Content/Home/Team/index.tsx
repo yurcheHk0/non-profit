2691d765f0713eb82e08b7fa725a9d7470ce181f
yurcheHk0 <yurcheHk0@gmail.com>
2022-12-04 00:15:20 +0200

    team component refactoring

--- src/components/Content/Home/Team/index.tsx
+++ src/components/Content/Home/Team/index.tsx
@@ -4,100 +4,27 @@ import { isMobile } from "react-device-detect";
 
 import styles from "./Team.module.scss";
 
+import { TeamMemberMapper } from "./members";
+
 export default function Team() {
     return <div id={'team'} className={`${styles.team} ${isMobile ? styles.mobile : ''}`}>
         <h2>Команда</h2>
-
+        
         <div className={styles.grid}>
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Денис Тарасов
+            {TeamMemberMapper?.map((member) => {
+                return <div className={styles.member}>
+                    <div 
+                        className={styles.icon}
+                        style={{backgroundImage: `url(${member?.icon!})`}}
+                     />
+                    <div className={styles.name}>
+                        {member.name}
+                    </div>
+                    <div className={styles.role}>
+                        {member.role}
+                    </div>
                 </div>
-                <div className={styles.role}>
-                    засновник
-                </div>
-            </div>
-
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Олексій Оленюк
-                </div>
-                <div className={styles.role}>
-                    роль
-                </div>
-            </div>
-
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Андрій Кіктенко
-                </div>
-                <div className={styles.role}>
-                    роль
-                </div>
-            </div>
-
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Олег Павленко
-                </div>
-                <div className={styles.role}>
-                    роль
-                </div>
-            </div>
-
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Олег Барило
-                </div>
-                <div className={styles.role}>
-                    роль
-                </div>
-            </div>
-
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Сергій Ніколаєв
-                </div>
-                <div className={styles.role}>
-                    роль
-                </div>
-            </div>
-
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Юрій Юрченко
-                </div>
-                <div className={styles.role}>
-                    роль
-                </div>
-            </div>
-
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Ірина Петрова
-                </div>
-                <div className={styles.role}>
-                    роль
-                </div>
-            </div>
-
-            <div className={styles.member}>
-                <div className={styles.iconDenis} />
-                <div className={styles.name}>
-                    Анна Баранова
-                </div>
-                <div className={styles.role}>
-                    дизайнер
-                </div>
-            </div>
+            })}
         </div>
     </div>
 }
